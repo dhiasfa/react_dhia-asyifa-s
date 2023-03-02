27376c6b80b8819fd40b45a4881f29ab5cd5a441
dhiasfa <salsabilamiqi@gmail.com>
2023-03-02 14:37:29 +0700

    initial commit

--- 10_Javascript Refreshment/Praktikum/TugasCreateProduct/assets/script.js
+++ 10_Javascript Refreshment/Praktikum/TugasCreateProduct/assets/script.js
@@ -15,29 +15,27 @@ function validateProductName() {
 }
 
 function handleInput(){
-  // document.querySelector('input[name="Pcategory"]:checked');
-  let productName = document.getElementById("productName").value;
-  // let productCategory = document.getElementById("Pcategory");
-  let productCategory = document.querySelector('input[name="Pcategory"]:checked');
+
+    let productName = document.getElementById("productName");
+    let productCategory = document.getElementById("Pcategory");
     let imageInput= document.getElementById("image-product");
     let productFreshness = document.querySelector('input[name="product_freshness"]:checked');
-    let productDescription = document.getElementById("product-description").value;
-    let productPrice = document.getElementById("product-price").value;
+    let productDescription = document.getElementById("product-description");
+    let productPrice = document.getElementById("product-price");
 
-  
-  const pattern = /[^\w\s]/gi;
-  if (pattern.test(productName)) {
-      alert("Name must not contain symbols.");
-      return false;
-  }
+    const pattern = /[^\w\s]/gi;
+    if (pattern.test(productName.value)) {
+        alert("Name must not contain symbols.");
+        return false;
+    }
 
   // Validasi input Product Name
-  if (productName ==="") {
+  if (productName.value.length === 0) {
       alert("The Product Name field must be filled in");
       return false;
   }
   // Validasi input Product Category
-  if (!productCategory) {
+  if (productCategory.value === "" ) {
       alert("The Product Category field must be filled in");
       return false;
   }
@@ -55,24 +53,24 @@ function handleInput(){
   }
 
   // Validasi input Additional Description
-  if (productDescription.length === 0) {
+  if (productDescription.value.length === 0) {
       alert("The Additional Description field must be filled in");
       return false;
   }
 
   // Validasi input Product Price
-  if (productPrice.length === 0) {
+  if (productPrice.value.length === 0) {
       alert("Please enter a valid Product price.");
       return false;
   }
-    const data = {
+  const data = {
       productName: productName.value,
       productCategory: productCategory.value,
       productFreshness: productFreshness.value,
       productDescription: productDescription.value,
       productPrice: productPrice.value
   };
+
   alert(`Data yang telah dimasukkan:\n${JSON.stringify(data, null, 2)}`);
+
 }
-
-
